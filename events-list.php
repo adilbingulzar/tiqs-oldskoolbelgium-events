fa403d2966d77cbe37114ae64258a77710b4e2ab
Adil Gulzar <adilbingulzar@gmail.com>
2023-06-09 17:48:11 +0500

    feedback

--- events-list.php
+++ events-list.php
@@ -8,7 +8,7 @@ class EventsList {
     }
 
     public function tiqs_events_list() {
-        wp_enqueue_script( 'tiqs-events-script-8', $this->toed_get_page_url('includes/js/custom.js'), array( 'jquery' ), null, true );
+        // wp_enqueue_script( 'tiqs-events-script-8', $this->toed_get_page_url('includes/js/custom.js'), array( 'jquery' ), null, true );
         wp_localize_script( 'tiqs-events-script-8', 'data', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
         wp_enqueue_style( 'tiqs-events-admin', plugin_dir_url( __FILE__ ) . 'style-admin.css' );
 
